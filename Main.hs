bb53044d9848da87e7362a576900deb324618575
Joe Bloggs <vapniks@yahoo.com>
2014-08-03 16:24:45 +0100

    Parallelized version
    Doesn't give much speedup (about 1.6x on 2 cores in my tests).
    Have tried a few different strategies from Control.Parallel.Strategies,
    but this seems best.
    Recursive application of parallel strategy produces too many sparks.
    Also tried setting a threshold on the length of validItems above which
    parallelism is used, but it didn't make much difference.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,13 @@ module Main where
 
 import Data.List
 import Data.Ord
+-- modules for parallel processing
+import Control.Parallel.Strategies
+import Control.DeepSeq
+-- modules for argument handling
+import System.Environment
+import System.Exit
+import System.IO
 
 ------------------------------------------------------------
 -- Utility.
@@ -32,21 +39,22 @@ largestSolution [] = []
 largestSolution is = maximumBy (tiebreak (comparing totalCost) (flip (comparing length))) is
 
 -- TODO : Memoize.
-knapsack :: Costed a => (a -> [a] -> [a]) -> Integer -> [a] -> [a]
-knapsack _ 0 _  = []
-knapsack _ _ [] = []
-knapsack prune w xs = largestSolution possibleSolutions
-  where validItems = filter (costWithin w) xs
-        possibleSolutions = fmap knapsackWithout validItems
-        knapsackWithout i = i : knapsack prune (w - cost i) (prune i validItems)
+knapsack :: Costed a => (a -> [a] -> [a]) -> Integer -> [a] -> Strategy [[a]] -> [a]
+knapsack _ _ [] _ = []
+knapsack prune w xs strategy = case (w > 0) of
+                                 True -> largestSolution possibleSolutions
+                                 False -> []
+         where validItems = filter (costWithin w) xs 
+               possibleSolutions = map knapsackWithout validItems `using` strategy
+               knapsackWithout i = i : knapsack prune (w - cost i) (prune i validItems) (evalList rseq)
 
 -- | Unbounded Knapsack. There is an unlimited number of each item.
-uks :: Costed a => Integer -> [a] -> [a]
-uks = knapsack (flip const)
+uks :: Costed a => Integer -> [a] -> Strategy [[a]] -> [a]
+uks = knapsack (flip const) 
 
 -- | Bounded Knapsack. Items can only be consumed once.
-bks :: (Eq a, Costed a) => Integer -> [a] -> [a]
-bks = knapsack delete
+bks :: (Eq a, Costed a) => Integer -> [a] -> Strategy [[a]] -> [a]
+bks = knapsack delete 
 
 ------------------------------------------------------------
 -- Items to put in the knapsack
@@ -55,24 +63,47 @@ bks = knapsack delete
 data Item = Item String Integer
               deriving (Read, Show, Eq)
 
+instance NFData Item where
+  rnf (Item s i) = rnf s `seq` rnf i
+
 instance Costed Item where
   cost (Item _ w) = w
 
 items :: [Item]
 items = [
-           Item "Beatles" 12,
-           Item "Bob Dylan" 23,
-           Item "Bangles" 3,
-           Item "Elton John" 9,
-           Item "Blue" 1
+           Item "1" 1,
+           Item "2" 2,
+           Item "3" 3,
+           Item "4" 4,
+           Item "5" 5,
+           Item "6" 6,
+           Item "7" 5,
+           Item "8" 4,
+           Item "9" 3,
+           Item "10" 2
         ]
 
  ------------------------------------------------------------
 
+-- main :: IO ()
+-- main = do
+--   print (totalCost b, b)
+--   print (totalCost u, u)
+--   where n = 30
+--         b = bks n items True
+--         u = uks n items True
+
 main :: IO ()
-main = do
-  print (totalCost b, b)
-  print (totalCost u, u)
-  where n = 30
-        b = bks n items
-        u = uks n items
+main = do args <- getArgs
+          case args of
+            ["t"] -> doit (parBuffer 100 rdeepseq)
+            ["f"] -> doit (evalList rseq)
+            _ -> do hPutStrLn stderr "Usage: ./Main t/f"
+                    exitWith $ ExitFailure 1 
+
+doit :: Strategy [[Item]] -> IO ()
+doit strategy = do print (totalCost b, b)
+                   print (totalCost u, u)
+                where n = 20
+                      b = bks n items strategy
+                      u = uks n items strategy
